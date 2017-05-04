a6e7d2b163913a5d9d5bef59d997ebeb5763179e
Taki Vlasakakis <taki.vlasakakis@tapinfluence.com>
2017-05-04 11:33:08 -0600

    ran 'nwb new react-app react-clock' hence the dir within dir

--- nwb.config.js
+++ nwb.config.js
@@ -1,3 +1,36 @@
+var path = require('path')
+
 module.exports = {
-  type: 'react-app'
+    babel: {
+        presets: ['stage-0'],
+    },
+    type: 'react-app',
+    webpack: {
+        html: {
+            favicon: 'public/favicon.png',
+        },
+        rules: {
+            css: {
+                modules: true,
+                localIdentName: '[local]_[hash:base64:5]',
+            },
+            postcss: {
+                plugins: [
+                    require('postcss-import')({
+                        path: ['src/styles'],
+                    }),
+                    require('postcss-cssnext')(),
+                    require('postcss-animation')(),
+                ],
+            },
+        },
+        aliases: {
+            'src': path.resolve('src'),
+            'components': path.resolve('src/components'),
+            'containers': path.resolve('src/containers'),
+            'helpers': path.resolve('src/helpers'),
+            'images': path.resolve('images'),
+            'services': path.resolve('src/services'),
+        },
+    },
 }
