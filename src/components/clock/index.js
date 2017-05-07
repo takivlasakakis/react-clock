1e75717be84268980c2f4d5f42bde2c6708dc7c1
Taki Vlasakakis <taki.vlasakakis@tapinfluence.com>
2017-05-06 20:05:49 -0600

    update lifecycle methods of the clock. will now mount and unmount

--- src/components/clock/index.js
+++ src/components/clock/index.js
@@ -2,40 +2,38 @@ import styles from './style.css'
 import React, { Component, PropTypes } from 'react'
 import { render } from 'react-dom'
 
-
-class Clock extends Component {
-    constructor() {
-        super()
-        // set initial time:
-        this.state = {
-            time: Date.now()
-        }
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
     }
 
     componentDidMount() {
-        // update time every second
-        this.timer = setInterval(() => {
-                this.setState({ time: Date.now() })
-    }, 1000)
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
     }
 
     componentWillUnmount() {
-        // stop when not renderable
-        clearInterval(this.timer)
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
     }
 
     render() {
-        let time = new Date(this.state.time).toLocaleTimeString();
         return (
             <div>
-            <span>{ 'Inferno version: ' + version }</span>
-            <br/>
-            <span>{ time }</span>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
             </div>
-    )
+        );
     }
 }
-
 // render an instance of Clock into <body>:
 // render(<Clock />, document.getElementById('app'));
 export default Clock
